691c16f4096253e471a2354340cda49dda1bf48a
Dan Scott <dan@coffeecode.net>
2020-11-16 14:41:17 -0500

    Use \o format to maintain multi-line formatting
    Signed-off-by: Dan Scott <dan@coffeecode.net>

--- export_tables.sql
+++ export_tables.sql
@@ -1,5 +1,6 @@
 -- 1) export items
 
+--KEEP
 \o OCUL_LU_items.csv
 
 COPY (
@@ -43,8 +44,9 @@ TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
 \o
 
 --item notes
+\o OCUL_LU_item_notes.csv
 
-\copy (
+COPY (
   SELECT * 
   FROM asset.copy_note
   WHERE owning_copy IN (
@@ -59,12 +61,15 @@ TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
           AND owning_lib IN (105,113,130,104,108,103,132,151,131,150,107,117)
       )
   )
-) TO OCUL_LU_item_notes.csv delimiter ',' CSV header
--- COPY 385
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --item stat cats
+\o OCUL_LU_item_stats.csv
 
-\copy (
+COPY (
   SELECT s.name, e.value, c.owning_copy
   FROM asset.stat_cat s, asset.stat_cat_entry e, asset.stat_cat_entry_copy_map c
   WHERE s.id = e.stat_cat
@@ -81,10 +86,10 @@ TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
             AND owning_lib IN (105,113,130,104,108,103,132,151,131,150,107,117)
       )
   )
-) TO OCUL_LU_item_stats.csv delimiter ',' CSV header
--- COPY 29229
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
 
--- 3) export the patron data
+\o
 
 --patrons
 CREATE OR REPLACE VIEW conifer.usr_with_authname AS 
@@ -103,15 +108,22 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
   )
   ORDER BY au.id;
 
-\copy (
+--KEEP
+\o OCUL_LU_patron.csv
+
+COPY (
   SELECT *
   FROM conifer.usr_with_authname
   WHERE home_ou IN (105,113,130,104,108,103,132,151,131,150,107,117)
-) TO OCUL_LU_patron.csv delimiter ',' CSV header
--- COPY 10943
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron barcodes
-\copy (
+\o OCUL_LU_patron_barcode.csv 
+
+COPY (
   SELECT *
   FROM actor.card ac
   WHERE active IS TRUE
@@ -121,12 +133,15 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
       WHERE card = ac.id
       AND home_ou IN (105,113,130,104,108,103,132,151,131,150,107,117)
   )
-) TO OCUL_LU_patron_barcode.csv delimiter ',' CSV header
--- COPY 77728
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron settings
+\o OCUL_LU_patron_setting.csv
 
-\copy (
+COPY (
   SELECT *
   FROM actor.usr_setting aus
   WHERE EXISTS (
@@ -135,12 +150,15 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
     WHERE au.id = aus.usr
       AND home_ou IN (105,113,130,104,108,103,132,151,131,150,107,117)
   )
-) TO OCUL_LU_patron_setting.csv delimiter ',' CSV header
--- COPY 1297
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron notes
+\o OCUL_LU_patron_notes.csv
 
-\copy (
+COPY (
   SELECT *
   FROM actor.usr_note aun
   WHERE EXISTS (
@@ -149,12 +167,16 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
     WHERE au.id = aun.usr
       AND home_ou IN (105,113,130,104,108,103,132,151,131,150,107,117)
   )
-) TO OCUL_LU_patron_notes.csv delimiter ',' CSV header
--- COPY 1987
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron address
+--KEEP
+\o OCUL_LU_patron_address.csv
 
-\copy (
+COPY (
   SELECT *
   FROM actor.usr_address aua
   WHERE EXISTS (
@@ -163,12 +185,16 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
     WHERE au.id = aua.usr
       AND home_ou IN (105,113,130,104,108,103,132,151,131,150,107,117)
   )
-) TO OCUL_LU_patron_address.csv delimiter ',' CSV header
--- COPY 25395
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron stat cats
-\copy (
-  SELECT m.target_usr, st.name,ste.value
+\o OCUL_LU_preferred_language.csv
+
+COPY (
+  SELECT m.target_usr AS usr, st.name,ste.value
   FROM actor.stat_cat st, actor.stat_cat_entry ste, conifer.usr_with_authname u, actor.stat_cat_entry_usr_map m
   WHERE u.id = m.target_usr
   AND u.deleted IS FALSE
@@ -181,12 +207,15 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
     WHERE au.id = m.target_usr
     AND home_ou IN (105,113,130,104,108,103,132,151,131,150,107,117)
   )
-) TO OCUL_LU_patron.stat_cat.csv delimiter ',' csv header
--- 19299
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron bookbags
+\o OCUL_LU_patron_bookbags.csv
 
-\copy (
+COPY (
   SELECT target_biblio_record_entry, barcode, name, title
   FROM container.biblio_record_entry_bucket c, container.biblio_record_entry_bucket_item i, reporter.super_simple_record r, conifer.usr_with_authname u, actor.card ca
   WHERE btype = 'bookbag'
@@ -196,23 +225,17 @@ CREATE OR REPLACE VIEW conifer.usr_with_authname AS
     AND owner = u.id
     AND u.card = ca.id
     ORDER BY owner, name, title
-) TO OCUL_LU_patron_bookbags.csv delimiter ',' csv header
--- 30775
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --export holds
 
 --check for holds WHERE the pickup lib is not an algoma one
 SELECT count(*) FROM action.hold_request WHERE request_lib IN (105,113,130,104,108,103,132,151,131,150,107,117) AND pickup_lib not IN (105,113,130,104,108,103,132,151,131,150,107,117) AND cancel_time IS NULL AND fulfillment_time IS NULL;
--- count 
----------
---     0
---(1 row)
 
--- conifer=# SELECT count(*) FROM action.hold_request WHERE request_lib IN (105,113,130,104,108,103,132,151,131,150,107,117) AND cancel_time IS NULL AND fulfillment_time IS NULL AND (expire_time > now() or expire_time IS NULL);
--- count 
----------
---    17
-
+--KEEP
 \o OCUL_LU_hold.csv
 
 COPY (
@@ -228,31 +251,27 @@ COPY (
 ) TO STDOUT (DELIMITER ',', FORMAT CSV, HEADER TRUE);
 
 \o
--- COPY 17
 
 --export circ
+--KEEP
+\o OCUL_LU_circ.csv
 
-\copy (
+COPY (
   SELECT ac.*, aou.name AS libraryname
   FROM action.circulation ac
     INNER JOIN actor.org_unit aou ON ac.circ_lib = aou.id
   WHERE circ_lib IN (105,113,130,104,108,103,132,151,131,150,107,117)
   AND checkin_time IS NULL
-) TO OCUL_LU_circ.csv delimiter ',' CSV header
--- COPY 3398
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --fines
 
 SELECT count(*) FROM money.billable_xact_summary_location_view  WHERE billing_location IN (105,113,130,104,108,103,132,151,131,150,107,117) AND balance_owed  != 0 ;
--- count 
----------
---  5381
---(1 row)
 
 SELECT count(distinct usr) FROM money.billable_xact_summary_location_view  WHERE billing_location IN (105,113,130,104,108,103,132,151,131,150,107,117) AND balance_owed != 0 ;
---  count 
--- -------
---    2338
 
 DROP TABLE mlb.laurentian_fines;
 CREATE TABLE mlb.laurentian_fines AS
@@ -262,7 +281,6 @@ CREATE TABLE mlb.laurentian_fines AS
   WHERE billing_location IN (105,113,130,104,108,103,132,151,131,150,107,117)
     AND balance_owed != 0
   ORDER BY usr;
--- SELECT 5377
 
 ALTER TABLE mlb.laurentian_fines add column title text;
 ALTER TABLE mlb.laurentian_fines add column barcode text;
@@ -274,44 +292,77 @@ WHERE a.id = ci.id
   AND ci.target_copy = co.id
   AND co.call_number = ca.id
   AND ca.record = r.id;
--- UPDATE 4032
 
 UPDATE mlb.laurentian_fines a
 SET barcode =  co.id
 FROM action.circulation ci, asset.copy co
 WHERE a.id = ci.id
 AND ci.target_copy = co.id ;
---UPDATE 4032
 
+--KEEP
+\o OCUL_LU_fines.csv
 
-\copy (SELECT * FROM mlb.laurentian_fines WHERE balance_owed > 0 ORDER BY usr) TO OCUL_LU_fines.csv delimiter ',' CSV header
--- COPY 5377
+COPY (
+  SELECT * FROM mlb.laurentian_fines WHERE balance_owed > 0 ORDER BY usr
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --codes
 
 --org units
-\copy (SELECT * FROM actor.org_unit ORDER BY id) TO OCUL_LU_org.csv delimiter ',' CSV header
--- COPY 49
+\o OCUL_LU_org.csv
+
+COPY (SELECT * FROM actor.org_unit ORDER BY id) TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --patron profile types
-\copy (SELECT * FROM permission.grp_tree ORDER BY id) TO OCUL_LU_patrontype.csv delimiter ',' CSV header
--- COPY 35
+\o OCUL_LU_patrontype.csv
+
+COPY (
+  SELECT * FROM permission.grp_tree ORDER BY id
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --item circ modifiers
-\copy (SELECT * FROM config.circ_modifier ORDER BY code) TO OCUL_LU_item_circ_modifiers.csv delimiter ',' CSV header
--- COPY 90
+\o OCUL_LU_item_circ_modifiers.csv
+
+COPY (
+  SELECT * FROM config.circ_modifier ORDER BY code
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --item locations
-\copy (SELECT * FROM asset.copy_location ORDER BY owning_lib, name) TO OCUL_LU_item_location.csv delimiter ',' CSV header
--- COPY 815
+\o OCUL_LU_item_location.csv
+COPY (
+  SELECT * FROM asset.copy_location ORDER BY owning_lib, name
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 
 --item status
-\copy (SELECT * FROM config.copy_status) TO OCUL_LU_item_status.csv delimiter ',' CSV header
--- COPY 20
+\o OCUL_LU_item_status.csv
+
+COPY (
+  SELECT * FROM config.copy_status
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+
+\o
 
 --P2E
-\copy (
+--KEEP
+\o OCUL_LU_p2e.csv
+
+COPY (
   SELECT acn.record, 'Portfolio' AS resource_type
   FROM asset.call_number acn
     INNER JOIN biblio.record_entry bre ON bre.id = acn.record
@@ -320,5 +371,8 @@ AND ci.target_copy = co.id ;
     AND acn.owning_lib IN (105,113,130,104,108,103,132,151,131,150,107,117)
     AND acn.label = '##URI##'
   ORDER BY acn.record
-) TO OCUL_LU_p2e.csv DELIMITER ',' CSV header
+)
+TO STDOUT WITH (DELIMITER ',', FORMAT CSV, HEADER FALSE);
+
+\o
 
